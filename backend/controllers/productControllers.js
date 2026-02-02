4d8bc107c3cddc7634b49aa1c5b535864c505e00
Sneha Maurya <snehamaurya2102@gmail.com>
2026-02-03 02:12:22 +0530

    Added error validation for invalid mongoose inputs

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,3 +1,4 @@
+import catchAsynErrors from "../middlewares/catchAsynErrors.js";
 import Product from "../modals/product.js"
 import ErrorHandler from "../utils/errorHandler.js"
 export const getProducts = async (req,res)=>{
@@ -8,14 +9,15 @@ const products =await Product.find();
     })
 }
 //create new product =>/api/v1/admin/products
-export const newProduct = async (req,res)=>{
+export const newProduct = catchAsynErrors(async (req,res)=>{
    const product = await Product.create(req.body);
 
    res.status(200).json({product,})
 }
+)
 
 //get product by id  =>/api/v1/products/:id
-export const productById = async (req,res , next)=>{
+export const productById =catchAsynErrors( async (req,res , next)=>{
    const product = await Product.findById(req?.params?.id);
 
    if(!product){
@@ -23,28 +25,30 @@ export const productById = async (req,res , next)=>{
       return next(new ErrorHandler("Product not found" , 404));
    }
    res.status(200).json({product,})
-}
+});
 
 //update product by id  =>/api/v1/admin/products/:id
-export const updateProduct = async (req,res)=>{
+export const updateProduct = catchAsynErrors(async (req,res)=>{
    let product = await Product.findById(req?.params?.id);
 
    if(!product){
-      return res.status(404).json({error:"Product not found"});
+       return next(new ErrorHandler("Product not found" , 404));
    }
 
    product = await Product.findByIdAndUpdate(req?.params?.id , req.body , {new:true})
    res.status(200).json({product,})
 }
+)
 //delete product by id  =>/api/v1/admin/products/:id
 
-export const deleteProduct = async (req,res)=>{
+export const deleteProduct = catchAsynErrors(async (req,res)=>{
    const product = await Product.findById(req?.params?.id);
 
    if(!product){
-      return res.status(404).json({error:"Product not found"});
+       return next(new ErrorHandler("Product not found" , 404));
    }
 
   await product.deleteOne();
    res.status(200).json({message:"Product is deleted"})
 }
+)
--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,9 +1,18 @@
+import ErrorHandler from "../utils/errorHandler.js";
+
 export default (err , req, res,next)=>{
     let error = {
         statusCode:err.statusCode ||500,
         message:err?.message || "Internel Server Error" , 
 
     };
+
+    //handle invalid mongoose id error
+    if(err.name==='CastError')
+    {
+        const message = `Resource not found . Invalid :${err?.path}`
+        error = new ErrorHandler(message, 404)
+    }
 
     if(process.env.NODE_ENV==='DEVELOPMENT'){
           res.status(error.statusCode).json({
