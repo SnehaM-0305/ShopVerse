f727f0cba33c02ff01db8bc57254d8f38e562c9c
Sneha Maurya <snehamaurya2102@gmail.com>
2026-02-07 23:55:59 +0530

    Added user model

---
+++ backend/modals/user.js
@@ -0,0 +1,36 @@
+import mongoose  from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String , 
+        required:[true,"Please enter your name"] , 
+        maxlength :[50 , 'Your name cannot exceed 50 characters'] , 
+        
+
+    } , 
+    email:{
+        type:String , 
+        required:[true , "Please enter your Email id "] , 
+        unique:true 
+    } , 
+    password:{
+        type:String , 
+        required:[true , "Please enter your password"],
+        minlength :[6,"Password should be longer than or equal to 6 character"],
+        select:false //dont want to send password in response 
+    },
+    avatar:{
+        public_id :String , 
+        url:String
+    },
+    role:{
+        type:String,
+        default:"user" , 
+
+    },
+   resetPasswordToken:String , 
+   resetPasswordExpire:Date ,
+
+},{timestamps:true})
+
+export default mongoose.model("User" , userSchema) ;
