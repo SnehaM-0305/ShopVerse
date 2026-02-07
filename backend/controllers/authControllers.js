e87765282c068ff244bfea7fc42e096d83bd83ab
Sneha Maurya <snehamaurya2102@gmail.com>
2026-02-08 01:16:39 +0530

    Added login route and logic for user

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,8 @@
 import catchAsyncErrors from "../middlewares/catchAsynErrors.js" ; 
-import User from "../modals/user.js" ; 
+import User  from "../modals/user.js"; 
+import ErrorHandler from "../utils/errorHandler.js"
+
+//register user /api/v1/register
 export const registerUser = catchAsyncErrors( async (req,res,next)=>{
     const {name ,  email , password} = req.body ; 
 
@@ -7,6 +10,33 @@ export const registerUser = catchAsyncErrors( async (req,res,next)=>{
     const user = await User.create({
         name , email,password 
     });
+
+    const token = user.getJwtToken()
+    res.status(200).json({
+        token,
+    })
+})
+
+//login user /api/v1/login
+export const loginUser = catchAsyncErrors( async (req,res,next)=>{
+    const {email , password} = req.body ; 
+
+   if(!email ||!password){
+    return next(new ErrorHandler('Please enter email and password' , 400))
+   }
+
+   //find user in the database 
+   const user = await User.findOne({email}).select("+password")
+
+   if(!user){
+    return next(new ErrorHandler("Invalid email or password" , 401))
+   }
+
+   //check if password is correct 
+   const isPasswordMatch = await user.comparePassword(password);
+   if(!isPasswordMatch){
+    return next(new ErrorHandler("Wrong password or email ",401))
+   }
 
     const token = user.getJwtToken()
     res.status(200).json({
